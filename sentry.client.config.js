b32b88ed7475ad2a0fc5e9a3537f253ac9d893de
Rajith S <101592607+srajith-skimli@users.noreply.github.com>
2023-01-18 18:34:34 +0530

    Skp 882 stop sentry transactions from webapp health check (#14)
    * update sentry config
    
    * add health check api route

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -4,24 +4,28 @@
 
 import * as Sentry from "@sentry/nextjs";
 
-const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
+const SENTRY_DSN = process.env.NEXT_PUBLIC_SENTRY_DSN;
 const SENTRY_ENABLED = process.env.NEXT_PUBLIC_SENTRY_ENABLED;
-const SENTRY_TRACING_ORIGIN =
-  process.env.NEXT_PUBLIC_SENTRY_TRACING_ORIGIN || "https://app.dev.skimli.com";
+const SENTRY_TRACING_ORIGIN = process.env.NEXT_PUBLIC_SENTRY_TRACING_ORIGIN;
+const SENTRY_ENVIRONMENT = process.env.NEXT_PUBLIC_SENTRY_ENVIRONMENT;
+const SENTRY_TRACES_SAMPLE_RATE =
+  process.env.NEXT_PUBLIC_SENTRY_TRACES_SAMPLE_RATE || 0.2;
 
 console.log("SENTRY ENABLED: ", SENTRY_ENABLED);
 
 Sentry.init({
   enabled: SENTRY_ENABLED,
-  dsn:
-    SENTRY_DSN ||
-    "https://1df7bb4d70dd44f18ece51308e24ee3d@o1184211.ingest.sentry.io/4504277400289280",
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1.0,
-  environment: process.env.NEXT_PUBLIC_SENTRY_ENVIRONMENT,
+  dsn: SENTRY_DSN,
+  environment: SENTRY_ENVIRONMENT,
   integrations: [
     new Sentry.BrowserTracing({
       tracingOrigins: [SENTRY_TRACING_ORIGIN, /^\//],
     }),
   ],
+  tracesSampler: (ctx) => {
+    if (ctx?.location?.pathname == "/api/health") {
+      return 0.0;
+    }
+    return SENTRY_TRACES_SAMPLE_RATE;
+  },
 });
