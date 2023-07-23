d4201fe5ec38a18ea707eeea2d666e908507d4f1
faisal-skp <79752880+faisalcla@users.noreply.github.com>
2023-07-22 20:49:56 -0700

    SKP-919: AC.1.1.4.b - Added volume tool tip

--- src/pages/organizations/[id]/assets/[assetId]/clips/sidebar-audio-edit/sidebar-audio-edit.component.tsx
+++ src/pages/organizations/[id]/assets/[assetId]/clips/sidebar-audio-edit/sidebar-audio-edit.component.tsx
@@ -1,22 +1,22 @@
-import { Fragment, ChangeEvent, FocusEvent } from 'react';
-import { Box, Slider } from '@mui/material';
-import { useTranslation } from 'next-i18next';
-import { ExpandPanel } from '~/components/expand-panel/expand-panel.component';
-import { useAssets } from '~/store/assets.slice';
-import { VolumeIcon } from '~/icons/volumeIcon';
-import { style } from './sidebar-audio-edit.style';
-import { useAudioContext } from '~/providers/AudioContextProvider';
-import { TextField } from '~/components/text-field/text-field.component';
-import { formatSeconds } from '~/utils/formatSeconds';
+import { Fragment, ChangeEvent, FocusEvent } from "react";
+import { Box, IconButton, Slider, Tooltip } from "@mui/material";
+import { useTranslation } from "next-i18next";
+import { ExpandPanel } from "~/components/expand-panel/expand-panel.component";
+import { useAssets } from "~/store/assets.slice";
+import { VolumeIcon } from "~/icons/volumeIcon";
+import { style } from "./sidebar-audio-edit.style";
+import { useAudioContext } from "~/providers/AudioContextProvider";
+import { TextField } from "~/components/text-field/text-field.component";
+import { formatSeconds } from "~/utils/formatSeconds";
+import { InfoIcon } from "~/icons/infoIcon";
 
 interface Props {
   assetId: string;
 }
 
 export function SidebarAudioEdit(props: Props) {
-
   const { assetId } = props;
-  const { t } = useTranslation('editClips');
+  const { t } = useTranslation("editClips");
 
   const assets = useAssets();
   const clip = assets.getClip(assetId);
@@ -24,34 +24,33 @@ export function SidebarAudioEdit(props: Props) {
   const audioContext = useAudioContext();
 
   function handleVolumeChange(event: Event, value: number | number[]) {
-
     audioContext.audioNode?.gain.gain.setValueAtTime(value as number, 0);
     audioContext.videoNode?.gain.gain.setValueAtTime(1 - (value as number), 0);
 
-    assets.updateTimelineClip(assetId, clip?.uuid, timelineAudio!.sources?.id, { 
+    assets.updateTimelineClip(assetId, clip?.uuid, timelineAudio!.sources?.id, {
       asset: {
-        volume: value as number
-      }
+        volume: value as number,
+      },
     });
   }
 
   function handleTrimChange(event: ChangeEvent<HTMLInputElement>) {
-    assets.updateTimelineClip(assetId, clip?.uuid, timelineAudio!.sources?.id, { 
-      asset: { 
-        trim: parseInt(event.target.value)
-      } 
+    assets.updateTimelineClip(assetId, clip?.uuid, timelineAudio!.sources?.id, {
+      asset: {
+        trim: parseInt(event.target.value),
+      },
     });
   }
 
-  function handleStartChange(event: ChangeEvent<HTMLInputElement>) {    
-    assets.updateTimelineClip(assetId, clip?.uuid, timelineAudio!.sources?.id, { 
-      start: parseInt(event.target.value)
+  function handleStartChange(event: ChangeEvent<HTMLInputElement>) {
+    assets.updateTimelineClip(assetId, clip?.uuid, timelineAudio!.sources?.id, {
+      start: parseInt(event.target.value),
     });
   }
 
   function handleLengthChange(event: ChangeEvent<HTMLInputElement>) {
-    assets.updateTimelineClip(assetId, clip?.uuid, timelineAudio!.sources?.id, { 
-      length: parseInt(event.target.value)
+    assets.updateTimelineClip(assetId, clip?.uuid, timelineAudio!.sources?.id, {
+      length: parseInt(event.target.value),
     });
   }
 
@@ -60,43 +59,61 @@ export function SidebarAudioEdit(props: Props) {
   }
 
   function handleTrimBlur(event: FocusEvent<HTMLInputElement>) {
-
-    if (event.currentTarget.value === '') {
-      assets.updateTimelineClip(assetId, clip?.uuid, timelineAudio!.sources?.id, { 
-        asset: { 
-          trim: 0
-        } 
-      });
+    if (event.currentTarget.value === "") {
+      assets.updateTimelineClip(
+        assetId,
+        clip?.uuid,
+        timelineAudio!.sources?.id,
+        {
+          asset: {
+            trim: 0,
+          },
+        }
+      );
     }
 
     assets.syncTimeline(assetId, clip!.uuid);
   }
 
   function handleStartBlur(event: FocusEvent<HTMLInputElement>) {
-
-    if (event.currentTarget.value === '') {
-      assets.updateTimelineClip(assetId, clip?.uuid, timelineAudio!.sources?.id, { 
-        start: 0
-      });
+    if (event.currentTarget.value === "") {
+      assets.updateTimelineClip(
+        assetId,
+        clip?.uuid,
+        timelineAudio!.sources?.id,
+        {
+          start: 0,
+        }
+      );
     }
 
     assets.syncTimeline(assetId, clip!.uuid);
   }
 
   function handleLengthBlur(event: FocusEvent<HTMLInputElement>) {
-
-    if (event.currentTarget.value === '') {
-      assets.updateTimelineClip(assetId, clip?.uuid, timelineAudio!.sources?.id, { 
-        length: timelineAudio?.sources?.duration || 0
-      });
+    if (event.currentTarget.value === "") {
+      assets.updateTimelineClip(
+        assetId,
+        clip?.uuid,
+        timelineAudio!.sources?.id,
+        {
+          length: timelineAudio?.sources?.duration || 0,
+        }
+      );
     }
 
     assets.syncTimeline(assetId, clip!.uuid);
   }
 
   function trimAndLengthValidator(trim?: number) {
-    if (trim === undefined || isNaN(trim) || trim > timelineAudio!.sources!.duration!) {
-      return `Value must be between 0 and ${Math.round(timelineAudio!.sources!.duration!)}`;
+    if (
+      trim === undefined ||
+      isNaN(trim) ||
+      trim > timelineAudio!.sources!.duration!
+    ) {
+      return `Value must be between 0 and ${Math.round(
+        timelineAudio!.sources!.duration!
+      )}`;
     }
   }
 
@@ -108,59 +125,78 @@ export function SidebarAudioEdit(props: Props) {
 
   return (
     <ExpandPanel
-      sx={style.expandPanel} 
-      id='edit' 
-      title={t('sidebarAudio.edit.title')}>
-      {timelineAudio &&
+      sx={style.expandPanel}
+      id="edit"
+      title={t("sidebarAudio.edit.title")}
+    >
+      {timelineAudio && (
         <Fragment>
           <Box sx={style.titleContainer}>
             <Box sx={style.title}>{timelineAudio?.sources?.title}</Box>
-            <Box sx={style.duration}>&nbsp;- {formatSeconds(timelineAudio!.sources?.duration || 0)}</Box>
+            <Box sx={style.duration}>
+              &nbsp;- {formatSeconds(timelineAudio!.sources?.duration || 0)}
+            </Box>
           </Box>
-          <Box sx={style.fieldTitle}>Volume</Box>
+          <Box sx={style.fieldTitle}>
+            Volume
+            <Tooltip
+              sx={style.tooltip}
+              title="Set the volume for the audio clip between 0% and 100%"
+            >
+              <IconButton sx={style.iconButton}>
+                <InfoIcon sx={style.icon} />
+              </IconButton>
+            </Tooltip>
+          </Box>
           <Box sx={style.volumeField}>
-            <VolumeIcon sx={style.volumeIcon}/>
+            <VolumeIcon sx={style.volumeIcon} />
             <Slider
               sx={style.slider}
               min={0}
               max={1}
               step={0.01}
               value={timelineAudio.asset?.volume}
-              orientation='horizontal'
+              orientation="horizontal"
               onChange={handleVolumeChange}
-              onBlur={handleVolumeBlur}/>
+              onBlur={handleVolumeBlur}
+            />
           </Box>
           <Box sx={style.fieldTitle}>Trim Start</Box>
           <TextField
-            sx={style.textField} 
-            type='number'
-            value={timelineAudio.asset.trim?.toString() || ''}
+            sx={style.textField}
+            type="number"
+            value={timelineAudio.asset.trim?.toString() || ""}
             min={0}
             max={Math.round(timelineAudio.sources?.duration || 0)}
             errorMessage={trimAndLengthValidator(timelineAudio.asset.trim)}
             onChange={handleTrimChange}
-            onBlur={handleTrimBlur}/>
+            onBlur={handleTrimBlur}
+          />
           <Box sx={style.subTitle}>Timing</Box>
           <Box sx={style.fieldTitle}>Start</Box>
           <TextField
-            sx={style.textField} 
-            type='number'
+            sx={style.textField}
+            type="number"
             value={Math.floor(timelineAudio.start).toString()}
             errorMessage={startValidator(timelineAudio.start)}
             onChange={handleStartChange}
-            onBlur={handleStartBlur}/>
+            onBlur={handleStartBlur}
+          />
           <Box sx={style.fieldTitle}>Length</Box>
           <TextField
-            sx={style.textField} 
-            type='number'
+            sx={style.textField}
+            type="number"
             value={Math.round(timelineAudio.length).toString()}
             min={0}
             max={Math.round(timelineAudio.sources?.duration || 0)}
-            errorMessage={trimAndLengthValidator(Math.round(timelineAudio.length))}
+            errorMessage={trimAndLengthValidator(
+              Math.round(timelineAudio.length)
+            )}
             onChange={handleLengthChange}
-            onBlur={handleLengthBlur}/>
+            onBlur={handleLengthBlur}
+          />
         </Fragment>
-      }
+      )}
     </ExpandPanel>
   );
 }
--- src/pages/organizations/[id]/assets/[assetId]/clips/sidebar-audio-edit/sidebar-audio-edit.style.ts
+++ src/pages/organizations/[id]/assets/[assetId]/clips/sidebar-audio-edit/sidebar-audio-edit.style.ts
@@ -1,75 +1,92 @@
-import { Style, nestedStyle } from '~/utils/style';
+import { Style, nestedStyle } from "~/utils/style";
 
 export const style = Style({
   expandPanel: nestedStyle({
     content: {
-      display: 'flex',
-      flexDirection: 'column',
-      overflow: 'hidden',
-      padding: '0px 12px',
-      'div': {
-        flexShrink: 0
-      }
-    }
+      display: "flex",
+      flexDirection: "column",
+      overflow: "hidden",
+      padding: "0px 12px",
+      div: {
+        flexShrink: 0,
+      },
+    },
   }),
   titleContainer: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    overflow: 'hidden',
-    color: 'black',
-    fontSize: '14px',
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    overflow: "hidden",
+    color: "black",
+    fontSize: "14px",
     fontWeight: 500,
-    margin: '16px 0px 4px'
+    margin: "16px 0px 4px",
   },
   title: {
-    textOverflow: 'ellipsis',
-    whiteSpace: 'nowrap',
-    overflow: 'hidden'
+    textOverflow: "ellipsis",
+    whiteSpace: "nowrap",
+    overflow: "hidden",
   },
   duration: {
-    flexShrink: 0
+    flexShrink: 0,
   },
   fieldTitle: {
-    color: 'black',
-    fontSize: '14px',
+    color: "black",
+    fontSize: "14px",
     fontWeight: 500,
-    marginTop: '16px'
+    marginTop: "16px",
   },
   volumeField: {
-    display: 'flex',
-    alignItems: 'center',
-    marginTop: '4px'
+    display: "flex",
+    alignItems: "center",
+    marginTop: "4px",
   },
   volumeIcon: {
-    width: '20px',
-    height: '20px'
+    width: "20px",
+    height: "20px",
   },
   slider: {
-    height: '8px',
-    margin: '0px 8px',
-    '& .MuiSlider-thumb:before': {
-      boxShadow: 'none'
+    height: "8px",
+    margin: "0px 8px",
+    "& .MuiSlider-thumb:before": {
+      boxShadow: "none",
     },
-    '& .MuiSlider-thumb': {
-      width: '14px',
-      height: '14px',
-      border: '2px solid',
-      boxShadow: 'none',
-      borderColor: 'white'
-    }
+    "& .MuiSlider-thumb": {
+      width: "14px",
+      height: "14px",
+      border: "2px solid",
+      boxShadow: "none",
+      borderColor: "white",
+    },
   },
   textField: nestedStyle({
     container: {
-      width: '100%',
-      marginTop: '4px'
-    }
+      width: "100%",
+      marginTop: "4px",
+    },
   }),
   subTitle: {
-    color: 'black',
-    fontSize: '15px',
+    color: "black",
+    fontSize: "15px",
     fontWeight: 500,
-    textAlign: 'center',
-    margin: '24px 0px 0px'
-  }
+    textAlign: "center",
+    margin: "24px 0px 0px",
+  },
+  tooltip: {
+    maxWidth: "360px",
+    backgroundColor: "white",
+    color: "black",
+    fontSize: "12px",
+    fontWeight: "600",
+    boxShadow: 1,
+    padding: "8px",
+  },
+  iconButton: {
+    ml: "8px",
+    pl: "0px",
+  },
+  icon: {
+    width: "20px",
+    height: "20px",
+  },
 });
